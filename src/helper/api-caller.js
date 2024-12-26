3bcbd0fa552d4bfb8eb5eaab7233cf8ca801b79a
hphan <hungpt.se@gmail.com>
2024-12-26 22:07:18 +0700

    init

--- src/helper/api-caller.js
+++ src/helper/api-caller.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001";
+const API_URL = process.env.API_URL || "http://localhost:3001";
 
 export const get = async (path, params) => {
   return await axios
